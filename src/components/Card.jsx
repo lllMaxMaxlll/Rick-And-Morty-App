93ea8c547c6aea36c6e87d6b563c240b801191f2
lllmaxmaxlll <max.herr.88@gmail.com>
2022-11-21 15:48:04 -0300

    Fix card size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,7 @@ import { GrClose } from "react-icons/gr";
 export default function Card({ id, name, species, gender, image, onClose }) {
 	return (
 		<div className={styles.card}>
-			<button className={styles.button} onClick={() => onClose(id)}>
-				<GrClose />
-			</button>
-			<div className={styles.cardImg}>
-				<img src={image} alt={name} />
-			</div>
+			<img className={styles.cardImg} src={image} alt={name} />
 			<div className={styles.cardInfo}>
 				<h2 className={styles.title}>{name}</h2>
 				<div className={styles.subtitle}>
@@ -17,6 +12,9 @@ export default function Card({ id, name, species, gender, image, onClose }) {
 					<h2>{gender}</h2>
 				</div>
 			</div>
+			<button className={styles.button} onClick={() => onClose(id)}>
+				<GrClose />
+			</button>
 		</div>
 	);
 }
