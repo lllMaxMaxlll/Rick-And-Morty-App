6cec6cecf85194428bfe8dc34ca82d4c8ca23b6b
lllmaxmaxlll <max.herr.88@gmail.com>
2022-11-23 09:07:31 -0300

    Agregado boton back to home

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,10 +1,11 @@
 import style from "./css-modules/Detail.module.css";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Detail = (props) => {
 	const [character, setCharacter] = useState({});
 	const { detailId } = useParams();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		fetch(`https://rickandmortyapi.com/api/character/${detailId}`)
@@ -23,12 +24,17 @@ const Detail = (props) => {
 	}, [detailId]);
 
 	console.log(character);
+
+	const backToHome = () => {
+		navigate("/home");
+	};
 	return (
 		<div className={style.detail}>
 			<h1>{character.name}</h1>
 			<h1>{character.id}</h1>
 			<h1>{character.species}</h1>
 			<h1>{character.status}</h1>
+			<button onClick={() => backToHome()}>Volver al home</button>
 		</div>
 	);
 };
